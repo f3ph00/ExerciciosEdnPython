c2827034081bbaa9ce55f5816653d1b3be96557d
André Felipe <120676813+f3ph00@users.noreply.github.com>
2026-01-23 10:23:04 -0300

    Update gorjeta.py

--- Exercicios_5/gorjeta.py
+++ Exercicios_5/gorjeta.py
@@ -1,13 +1,13 @@
-'''
-1 - Crie uma função que calcule a gorjeta a ser deixada em um restaurante, baseada no valor total da conta e na porcentagem de
+'''1 - Crie uma função que calcule a gorjeta a ser deixada em um restaurante, baseada no valor total da conta e na porcentagem de
 gorjeta desejada. Calcula o valor da gorjeta baseado no total da conta e na porcentagem desejada.
 Parâmetros:
 a - valor_conta (float): O valor total da conta
 b - porcentagem_gorjeta (float): A porcentagem da gorjeta (ex: 10 para 10%)
-c - retorna: float: O valor da gorjeta calculada
-'''
+c - retorna: float: O valor da gorjeta calculada'''
 
-def gorjeta(valor_conta, porcentagem):
-    return (valor_conta * porcentagem) / 100
-
-print(gorjeta(1500,10))
+def calcular_gorjeta(valor_conta: float, porcentagem_gorjeta: float) -> float:
+    return valor_conta * (porcentagem_gorjeta / 100)
+valor = float(input("Digite o valor da conta: R$ "))
+porcentagem = float(input("Digite a porcentagem da gorjeta (%): "))
+gorjeta = calcular_gorjeta(valor, porcentagem)
+print(f"Gorjeta: R$ {gorjeta:.2f}")
